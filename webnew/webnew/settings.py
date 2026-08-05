cbf230d121de24e0c944c1f676fa5e4e6e3ca94d
Mishasay <mykhailosharov@gmail.com>
2026-08-05 22:07:14 +0200

    Fixed email sending

--- webnew/webnew/settings.py
+++ webnew/webnew/settings.py
@@ -123,16 +123,21 @@ STATICFILES_DIRS = [
     BASE_DIR / "main" / "static",
 ]
 
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+import os
+
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = "mykhailosharov@gmail.com"
-EMAIL_HOST_PASSWORD = "uuheblomrafhzdol"
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 CONTACT_EMAIL = "mykhailosharov@gmail.com"
-
-
