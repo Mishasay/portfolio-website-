561c9cc3b2ea20e066406b2564454b17e0f98cad
Mishasay <mykhailosharov@gmail.com>
2026-08-05 02:06:47 +0200

    Prepare Django project for deployment

--- webnew/webnew/settings.py
+++ webnew/webnew/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-f5t#mg=4ba(ma5fwfby-iq96ln1=8#_euexp2366mtd9vfm^eq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -116,9 +117,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    BASE_DIR / "main" / "static",
 ]
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
